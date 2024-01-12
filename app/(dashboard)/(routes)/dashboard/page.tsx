65d9e31218d883cc2f87ec9fdfd2b38650d920da
Vivek kr <vivekkr0004@gmail.com>
2024-01-12 23:03:17 +0530

    some changes

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import { UserButton } from '@clerk/nextjs';
+
+export default function DashboardPage() {
+  return (
+    <>
+        <p className="text-6xl">Hello Vivek</p>
+        <UserButton />
+    </>
+  )
+}
