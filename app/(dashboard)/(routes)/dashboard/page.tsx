2568979556396de9ef0a2c1f38e2056600e60c55
Vivek kr <vivekkr0004@gmail.com>
2024-01-16 23:01:18 +0530

    Added sidebar for mobile and web screens

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -3,8 +3,7 @@ import { UserButton } from '@clerk/nextjs';
 export default function DashboardPage() {
   return (
     <>
-        <p className="text-6xl">Hello Vivek</p>
-        <UserButton />
+        <p>Dashboard</p>
     </>
   )
 }
